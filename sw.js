a02e8ed9a0b8385e8c73c1d4be65a1bbbed25df9
gcviky1992 <gcviky1992@gmail.com>
2026-06-12 22:22:15 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yuanhenglizhen-phone-v1';
+const CACHE='yuanhenglizhen-phone-v2';
 const FILES=['./phone.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));

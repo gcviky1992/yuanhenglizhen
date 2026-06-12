0fd210cd4ab6ffea0ad6d206aeb6859afbda3868
gcviky1992 <gcviky1992@gmail.com>
2026-06-12 22:02:35 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE='yuanhenglizhen-phone-v1';
+const FILES=['./phone.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{
+    if(event.request.url.startsWith(self.location.origin)){
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    }
+    return response;
+  }).catch(async()=>{
+    const hit=await caches.match(event.request);
+    if(hit)return hit;
+    if(event.request.mode==='navigate')return caches.match('./phone.html');
+    throw new Error('offline');
+  }));
+});
